fecf9bb35c0293787f6319680a41d0aa3b49d824
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2023-06-06 10:28:51 +0900

    GameMainの修正

--- 2023SummerGameJam/GameMain.h
+++ 2023SummerGameJam/GameMain.h
@@ -1,48 +1,24 @@
 #pragma once
 #include"AbstractScene.h"
+#include "BlockManager.h"
+
 class GameMain :
     public AbstractScene
 {
 private:
+    BlockManager* block_manager;
 
-    enum class MENU
+
+    //遅延アニメーション用カウンタ
+    int delay_animation_count;
+
+    enum class DELAY_ANIMATION_TYPE
     {
-        PLAY,
-        //OPTION,
-        HELP,
-        EXIT,
-        MENU_SIZE
+        FADE_IN,
+        FADE_OUT,
+        DELAY_ANIMATION_TYPE_SIZE
     };
 
-    const char* menu_items[static_cast<int>(MENU::MENU_SIZE)] = {
-        "PLAY",
-        //"OPTION",
-        "HELP",
-        "EXIT"
-    };
-
-    // 選択しているメニュー
-    int select_menu;
-
-
-#ifdef TITLE_DEBUG
-    // デバックメニュー選択用フラグ
-    bool is_select_debug;
-#endif // TITLE_DEBUG
-
-    // フォントハンドル
-    int menu_font;
-
-    //選択SE用サウンドハンドル
-    int select_se;
-    //決定SE用サウンドハンドル
-    int decision_se;
-
-    //操作間隔時間
-    int input_margin;
-
-    //フェード用カウンタ
-    int fade_counter;
 
 public:
 
@@ -57,5 +33,12 @@ public:
 
     //描画に関することを実装
     void Draw() const override;
-};
 
+
+        /// <summary>
+    /// シーン切替前の遅延アニメーション
+    /// </summary>
+    /// <returns></returns>
+    /// <remarks>シーン切替前に遅延アニメーションを行う</remarks>
+    bool DelayAnimation(DELAY_ANIMATION_TYPE type, float time);
+};
